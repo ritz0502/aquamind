2ff9575790a4fb0410df05aab345b7012e2bb38d
Reetika Gupta <reetikagupta05@gmail.com>
2025-11-06 15:07:56 +0530

    navbar and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,35 @@
+// import AquaMind from "./pages/AquaMind";
+
+// function App() {
+//   return <AquaMind />;
+// }
+
+// export default App;
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 import AquaMind from "./pages/AquaMind";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
 
 function App() {
-  return <AquaMind />;
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<AquaMind />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
+
